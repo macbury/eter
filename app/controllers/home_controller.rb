e2b51014688a361f85cb41a7fc83c5888a0262a6
Arkadiusz Buras <macbury@gmail.com>
2014-11-21 15:58:08 +0100

    Dashboard controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
-  before_action :authenticate_user!
-  
+
   def index
+    if user_signed_in?
+      redirect_to dashboard_path
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get '/dashboard' => "dashboard#index", as: :dashboard
+
   devise_for :users
 
   root "home#index"
